977e1c20cd616251e51041a367987eb1e582e8d9
João Victor <joaovictordasilva0911@gmail.com>
2025-06-28 22:58:04 -0300

    feat: add Vitest for testing and implement e2e tests
    - Added Vitest commands to package.json for running tests, watching, coverage, and debugging.
    - Updated dependencies to include Vitest and related plugins.
    - Created a new FetchRecentQuestionsController for fetching recent questions with pagination.
    - Implemented Zod validation in controllers for request validation.
    - Added a new e2e test file for create-account controller.
    - Refactored import paths to use aliasing for better readability.
    - Updated ZodValidationPipe to handle custom metadata types.
    - Created Vitest configuration files for unit and e2e testing.

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,17 +1,53 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
-import { CurrentUser } from 'src/auth/current-user-decorator'
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
-import { UserPayload } from 'src/auth/jwt.strategy'
+import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
+import { CurrentUser } from '@/auth/current-user-decorator'
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
+import { UserPayload } from '@/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { PrismaService } from '@/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor() {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Post()
-  async handle(@CurrentUser() user: UserPayload) {
-    console.log(user)
+  @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
+  async handle(
+    @Body() body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body
+    const userId = user.sub
 
-    return 'ok'
+    await this.prisma.question.create({
+      data: {
+        title,
+        content,
+        slug: this.convertToSlug(title),
+        authorId: userId,
+      },
+    })
+  }
+
+  private convertToSlug(text: string) {
+    const slugText = text
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
+
+    return slugText
   }
 }
