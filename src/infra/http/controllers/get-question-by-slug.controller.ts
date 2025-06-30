b2a21200dd74ba00a4407b10e41986e05f4d2804
João Victor <joaovictordasilva0911@gmail.com>
2025-06-30 14:46:49 -0300

    feat: implement GetQuestionBySlug use case and controller with E2E tests

---
+++ src/infra/http/controllers/get-question-by-slug.controller.ts
@@ -0,0 +1,35 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+} from '@nestjs/common'
+import { QuestionPresenter } from '../presenters/question-presenter'
+import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+
+@Controller('/questions/:slug')
+export class GetQuestionBySlugController {
+  constructor(private readonly getQuestionBySlug: GetQuestionBySlugUseCase) {}
+
+  @Get()
+  async handle(@Param('slug') slug: string) {
+    const result = await this.getQuestionBySlug.execute({
+      slug,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+
+      switch (error.constructor) {
+        case ResourceNotFoundError:
+          throw new NotFoundException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
+    }
+
+    return { question: QuestionPresenter.toHTTP(result.value.question) }
+  }
+}
